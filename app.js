034433c255d9acbc9d25fe7435f592d008d55977
EvgHrn <evg.hrn@gmail.com>
2020-02-25 17:14:47 +0400

    bodyParser depr

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(bodyParser);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
