50d285b8776df20f5be9309a07bb45eb76864ddd
EvgHrn <evg.hrn@gmail.com>
2020-02-26 15:28:39 +0400

    Get avatar

--- app.js
+++ app.js
@@ -663,10 +663,7 @@ const restAuth = async (auth) => {
 }
 
 const base64_encode = (file) => {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new Buffer(bitmap).toString('base64');
+    return fs.readFileSync(file, { encoding: 'base64' });
 }
 
 /**
