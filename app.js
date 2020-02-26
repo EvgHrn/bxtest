ede3f4116b409ae310fce6d57d4d4266f76f2803
EvgHrn <evg.hrn@gmail.com>
2020-02-26 12:10:45 +0400

    support users

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const supportGroup = ["1819", "1514"];
+const supportGroup = ["1819", "1514", "3"];
 
 let config;
 
